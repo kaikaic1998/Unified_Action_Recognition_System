a9cf196f9adf8a73ae7c8852abe91d5feed68042
kaikaic1998 <kainianc@gmail.com>
2023-09-14 21:43:36 +0900

    add

--- caogao.py
+++ caogao.py
@@ -1,51 +1,5 @@
-import torch
-import cv2
-from torchvision import transforms
-import numpy as np
-import time
+import json
 
-from utils.datasets import letterbox
-from utils.general import non_max_suppression_kpt
-from utils.plots import output_to_keypoint, plot_skeleton_kpts
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-weigths = torch.load('yolov7-w6-pose.pt', map_location=device)
-model = weigths['model']
-_ = model.float().eval()
-
-print('device: ', device)
-
-if torch.cuda.is_available():
-    model.half().to(device)
-
-cap = cv2.VideoCapture(0)
-
-while True:
-    check, image = cap.read()
-
-    # image = cv2.imread('./inference/images/zidane.jpg')
-    image = letterbox(image, 960, stride=64, auto=True)[0]
-    image_ = image.copy()
-    image = transforms.ToTensor()(image)
-    image = torch.tensor(np.array([image.numpy()]))
-
-    if torch.cuda.is_available():
-        image = image.half().to(device)   
-    output, _ = model(image)
-
-    output = non_max_suppression_kpt(output, 0.25, 0.65, nc=model.yaml['nc'], nkpt=model.yaml['nkpt'], kpt_label=True)
-    with torch.no_grad():
-        output = output_to_keypoint(output)
-    nimg = image[0].permute(1, 2, 0) * 255
-    nimg = nimg.cpu().numpy().astype(np.uint8)
-    nimg = cv2.cvtColor(nimg, cv2.COLOR_RGB2BGR)
-    for idx in range(output.shape[0]):
-        plot_skeleton_kpts(nimg, output[idx, 7:].T, 3)
-
-    cv2.imshow('0', nimg)
-    cv2.waitKey(1)
-
-    if cv2.waitKey(1) == ord('q'):  # q to quit
-        break
-cap.release()
-cv2.destroyAllWindows()
+annotation_file_path = '../COCO/annotations/person_keypoints_train2017.json'
+annotation = json.load(open(annotation_file_path))
+print(annotation)
